642fc62aef1d946fbf70d15eb0535de4f5b5baf0
TobiH-GE <tobias.hartwich@hotmail.de>
2021-01-19 18:05:52 +0100

    added test CreationOverloadWithoutName()

--- FussballManagerTest/PlayerTests.cs
+++ FussballManagerTest/PlayerTests.cs
@@ -18,5 +18,13 @@ namespace FussballManagerTest
             Player p = new Player {Name = "Hans", Speed = 50, Precision = 50, Duel = 50, Position = PlayerPositions.Keeper };
             Assert.IsNotNull(p);
         }
+
+        [TestMethod]
+        public void CreationOverloadWithoutName()
+        {
+            Player p = new Player {Speed = 50, Precision = 50, Duel = 50, Position = PlayerPositions.Keeper };
+            Assert.IsNotNull(p);
+            Assert.IsFalse(p.Name == "");
+        }
     }
 }
