7e43814d7d5ffbd0c3f7ac5e33de16c1e104b7f4
Lutz <lambda2@gmx.at>
2021-01-19 17:03:32 +0100

    added saison

---
+++ FussballManagerLogic/Saison.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FussballManagerLogic
+{
+    public class Saison
+    {
+        public List<Match> Matches { get; set; }
+
+        public Saison()
+        {
+            Matches = new List<Match>();
+        }
+
+        public void CalculateDay(int v)
+        {
+            foreach (var item in Matches)
+            {
+                if (item.Day == v)
+                {
+                    item.CalculateResult();
+                }
+            }
+        }
+    }
+}
---
+++ FussballManagerTest/SaisonTests.cs
@@ -0,0 +1,53 @@
+using FussballManagerLogic;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Serialization;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace FussballManagerTest
+{
+    [TestClass]
+    public class SaisonTests
+    {
+        [TestMethod]
+        public void Creation()
+        {
+            var s = new Saison();
+            Assert.IsNotNull(s);
+            Assert.IsNotNull(s.Matches);
+        }
+
+        [TestMethod]
+        public void FillSaison()
+        {
+            Saison s = new();
+            Team tA = Helper.CreateTeam();
+            Team tB = Helper.CreateTeam();
+            
+            Match m = new(tA, tB);
+            Match n = new(tB, tA);
+            m.Day = 1;
+            n.Day = 2;
+            s.Matches.Add(m);
+            s.Matches.Add(n);
+            s.CalculateDay(1);
+            Assert.IsTrue(s.Matches[0].Home == 0);
+            Assert.IsTrue(s.Matches[0].Visitor == 0);
+        }
+
+        [TestMethod]
+        public void FullSeason()
+        {
+            List<Team> Teams = new List<Team>();
+            for (int counter = 0; counter < 18; counter++)
+                Teams.Add(Helper.CreateTeam());
+
+            Saison s = new();
+            for (int outer = 1; outer < Teams.Count; outer++)
+                for (int inner = 0; inner < Teams.Count; inner++)
+                    s.Matches.Add(new Match(Teams[inner], Teams[(inner + outer) % Teams.Count]) {Day = outer});
+
+            //TODO: add assert
+
+        }
+    }
+}
