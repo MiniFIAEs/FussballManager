d322bfcab1a3f489e1b4f8918a11ffdadf6ab554
TobiH-GE <tobias.hartwich@hotmail.de>
2021-01-19 17:56:58 +0100

    added Player(...) constructor without player name, added GetRandomName()

--- FussballManagerLogic/Player.cs
+++ FussballManagerLogic/Player.cs
@@ -1,4 +1,7 @@
-﻿namespace FussballManagerLogic
+﻿using System;
+using System.IO;
+
+namespace FussballManagerLogic
 {
     public class Player
     {
@@ -7,7 +10,6 @@
         public byte Precision { get; set; }
         public byte Duel { get; set; }
         public PlayerPositions Position { get; set; }
-
 
         public Player()
         {
@@ -21,6 +23,33 @@
             Precision = pPrecision;
             Duel = pDuel;
             Position = pPosition;
+        }
+        public Player(byte pSpeed, byte pPrecision, byte pDuel, PlayerPositions pPosition)
+        {
+            Name = GetRandomName();
+            Speed = pSpeed;
+            Precision = pPrecision;
+            Duel = pDuel;
+            Position = pPosition;
+        }
+        public string GetRandomName() //TODO: Datei nur einmal in eine List laden, daraus Namen entnehmen und löschen. Im Moment wird für jeden Player immmer wieder die Datei geöffnet und doppelte Namen sind möglich
+        {
+            // Liste "RandomNames.txt" muss mit in das Projektverzeichnis kopiert werden, generiert mit http://names.drycodes.com/1000?nameOptions=boy_names&format=text
+            Random rndGen = new Random();
+            int r = rndGen.Next(1000);
+            int count = 0;
+
+            using (var reader = new StreamReader("RandomNames.txt"))
+            {
+                string name;
+                while ((name = reader.ReadLine()) != null)
+                {
+                    if (count == r) return name;
+                    count++;
+                }
+            }
+
+            return "Error";
         }
     }
 }
