95c403443a9646c34929390009499f386a77d8cb
TobiH-GE <tobias.hartwich@hotmail.de>
2021-01-24 13:08:17 +0100

    only one Teamname needed

--- FussballManagerLogic/Standing.cs
+++ FussballManagerLogic/Standing.cs
@@ -10,7 +10,7 @@ namespace FussballManagerLogic
     public class Standing
     {
         public int Games { get; set; }
-        public Team TeamObject { get; set; }
+        public string TeamName { get; set; }
         public int Points { get; set; }
         public int GoalsHome { get; set; }
         public int GoalsVisitor { get; set; }
