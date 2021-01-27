abd3e1680eb21c5a8358853c009bebd5661d2aa9
TobiH-GE <tobias.hartwich@hotmail.de>
2021-01-27 23:35:43 +0100

    season corrected

--- FussballManagerTest/Helper.cs
+++ FussballManagerTest/Helper.cs
@@ -34,16 +34,35 @@ namespace FussballManagerTest
 
             return s;
         }
-        public static Saison CreateAndPlaySeason() //TODO: correct season
+        public static Saison CreateAndPlaySeason() //TODO: optimize loops
         {
             List<Team> Teams = new List<Team>();
             for (int counter = 0; counter < 18; counter++)
                 Teams.Add(Helper.CreateTeam("Team_" + counter));
 
             Saison s = new();
-            for (int outer = 1; outer < Teams.Count; outer++)
-                for (int inner = 0; inner < Teams.Count; inner++)
-                    s.Matches.Add(new Match(Teams[inner], Teams[(inner + outer) % Teams.Count]) { Day = outer });
+
+            int day = 1;
+            bool sw = true;
+
+            for (int offset = 1; offset <= (Teams.Count - 1) * 2; offset += 2)
+            {
+                for (int home = 0; home < Teams.Count; home += 2)
+                {
+                    if (sw) // switch home and visitor (day1: teamA = home, day2: teamA = visitor, day3: teamA = home, ...)
+                    {
+                        s.Matches.Add(new Match(Teams[home], Teams[(home + offset) % Teams.Count]) { Day = day });
+                        s.Matches.Add(new Match(Teams[(home + offset) % Teams.Count], Teams[home]) { Day = day + 17 }); // second half of the season
+                    }
+                    else
+                    {
+                        s.Matches.Add(new Match(Teams[(home + offset) % Teams.Count], Teams[home]) { Day = day });
+                        s.Matches.Add(new Match(Teams[home], Teams[(home + offset) % Teams.Count]) { Day = day + 17 });
+                    }
+                }
+                sw = !sw;
+                day++;
+            }
 
             foreach(Match match in s.Matches)
             {
